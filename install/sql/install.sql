e2dc35f2c8f432cb88b26a36e3755f6b1a470044
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-04-12 12:30:14 +0200

    fix install sql

--- install/sql/install.sql
+++ install/sql/install.sql
@@ -1,11 +1,12 @@
-DROP TABLE IF EXISTS `members_restrictions`;
 CREATE TABLE `members_restrictions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `targetId` int(11) unsigned NOT NULL DEFAULT '0',
+  `ctype` varchar(255) DEFAULT NULL,
   `inheritable` tinyint(1) unsigned DEFAULT '1',
   PRIMARY KEY (`id`),
-  KEY `targetId` (`targetId`,`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `unique_targetId_cType` (`targetId`,`ctype`),
+  KEY `index_targetId_cType` (`targetId`,`ctype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `members_group_relations`;
 CREATE TABLE `members_group_relations` (
