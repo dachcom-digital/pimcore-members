1375818113b4a5f8209391ff86fb96295a828704
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-04-06 11:09:49 +0200

    allow documents in redirectAfterSuccess area element

--- views/areas/member-login/edit.php
+++ views/areas/member-login/edit.php
@@ -2,25 +2,30 @@
 
     <div class="t-row">
         <label><?= $this->translateAdmin('Redirect after successful login') ?></label>
-        <?php
-        echo $this->href('redirectAfterSuccess', [
-            'types'   => ['document'],
+        <?= $this->href('redirectAfterSuccess', [
+            'types'    => ['document'],
+            'subtypes' => [
+                'document' => ['page', 'link', 'hardlink']
+            ]
         ]); ?>
+
     </div>
+
     <div class="t-row">
         <label><?= $this->translateAdmin('Hide when logged in') ?></label>
         <?= $this->checkbox('hideWhenLoggedIn'); ?>
     </div>
 
-    <?php if ( !$this->checkbox('hideWhenLoggedIn')->getData() ) { ?>
-    <div class="t-row">
-        <label><?= $this->translateAdmin('Show this snippet when logged in') ?></label>
-        <?php
-        echo $this->href('showSnippedWhenLoggedIn', [
-            'types'   => ['document'],
-            'subtypes'   => ['snippet'],
-        ]); ?>
-    </div>
+    <?php if (!$this->checkbox('hideWhenLoggedIn')->getData()) { ?>
+        <div class="t-row">
+            <label><?= $this->translateAdmin('Show this snippet when logged in') ?></label>
+            <?= $this->href('showSnippedWhenLoggedIn', [
+                'types'    => ['document'],
+                'subtypes' => [
+                    'document' => ['snippet']
+                ]
+            ]); ?>
+        </div>
     <?php } ?>
 
 </div>
