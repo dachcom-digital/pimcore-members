073506083829e8a99f25fd85b977b158e74706da
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-07-25 10:16:05 +0200

    take care about if a restriction is in place or not

--- src/MembersBundle/Controller/Admin/RestrictionController.php
+++ src/MembersBundle/Controller/Admin/RestrictionController.php
@@ -136,14 +136,15 @@ class RestrictionController extends AdminController
 
         //remove restriction since no group is selected any more.
         if (empty($settings->membersDocumentUserGroups)) {
-            if ($hasRestriction === true && !$restriction->getIsInherited()) {
-                $restriction->delete();
-            }
-            else {
-                $restriction->setInherit($membersDocumentInheritable);
-                $restriction->setIsInherited(false);
-                $restriction->setRelatedGroups($membersDocumentUserGroups);
-                $restriction->save();
+            if ($hasRestriction === true) {
+                if (!$restriction->getIsInherited()) {
+                    $restriction->delete();
+                } else {
+                    $restriction->setInherit($membersDocumentInheritable);
+                    $restriction->setIsInherited(false);
+                    $restriction->setRelatedGroups($membersDocumentUserGroups);
+                    $restriction->save();
+                }
             }
             //update or set restriction
         } else {
