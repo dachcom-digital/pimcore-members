8a6fd7d12f4be3b073da68d2a6b89f3bb9b87fb2
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-01-20 14:31:11 +0100

    add webservice tests

--- tests/_support/RestTester.php
+++ tests/_support/RestTester.php
@@ -1,22 +1,11 @@
 <?php
 
-namespace Pimcore\Tests;
+namespace DachcomBundle\Test;
 
 /**
- * Inherited Methods
+ * Class RestTester
  *
- * @method void wantToTest($text)
- * @method void wantTo($text)
- * @method void execute($callable)
- * @method void expectTo($prediction)
- * @method void expect($prediction)
- * @method void amGoingTo($argumentation)
- * @method void am($role)
- * @method void lookForwardTo($achieveValue)
- * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
- *
- * @SuppressWarnings(PHPMD)
+ * @package DachcomBundle\Test
  */
 class RestTester extends \Codeception\Actor
 {
---
+++ tests/bundle_tests/rest/ObjectTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Pimcore\Tests\Rest;
+
+use DachcomBundle\Test\Util\MembersHelper;
+use MembersBundle\Adapter\Group\GroupInterface;
+use MembersBundle\Adapter\User\UserInterface;
+use Pimcore\Model\DataObject\AbstractObject;
+use Pimcore\Model\DataObject\MembersGroup;
+use Pimcore\Model\DataObject\MembersUser;
+use Pimcore\Tests\Test\RestTestCase;
+use Pimcore\Tests\Util\TestHelper;
+
+class UserTest extends RestTestCase
+{
+    protected function _after()
+    {
+        TestHelper::cleanUp();
+        MembersHelper::cleanUp();
+        MembersHelper::reCreateMembersStructure();
+
+        parent::_after();
+    }
+
+    public function testUserCreate()
+    {
+        $unsavedObject = new MembersUser();
+        $unsavedObject->setParentId(1);
+        $unsavedObject->setUserOwner(1);
+        $unsavedObject->setUserModification(1);
+        $unsavedObject->setCreationDate(time());
+        $unsavedObject->setKey(uniqid() . rand(10, 99));
+
+        $time = time();
+
+        $result = $this->restClient->createObjectConcrete($unsavedObject);
+
+        $this->assertTrue($result->success, 'request not successful . ' . $result->msg);
+        $this->assertEquals(2, TestHelper::getObjectCount());
+
+        $id = $result->id;
+        $this->assertTrue($id > 1, 'id must be greater than 1');
+
+        $objectDirect = AbstractObject::getById($id);
+        $creationDate = $objectDirect->getCreationDate();
+
+        $this->assertTrue($creationDate >= $time, 'wrong creation date');
+
+        // as the object key is unique there must be exactly one object with that key
+        $list = $this->restClient->getObjectList('{ "o_key" : "' . $unsavedObject->getKey() . '"}');
+        $this->assertEquals(1, count($list));
+
+        $fetchedObject = $this->restClient->getObjectById($id);
+        $this->assertInstanceOf(UserInterface::class, $fetchedObject);
+    }
+
+    public function testUserGroupCreate()
+    {
+        $unsavedObject = new MembersGroup();
+        $unsavedObject->setParentId(1);
+        $unsavedObject->setUserOwner(1);
+        $unsavedObject->setUserModification(1);
+        $unsavedObject->setCreationDate(time());
+        $unsavedObject->setKey(uniqid() . rand(10, 99));
+
+        $time = time();
+
+        $result = $this->restClient->createObjectConcrete($unsavedObject);
+
+        $this->assertTrue($result->success, 'request not successful . ' . $result->msg);
+        $this->assertEquals(2, TestHelper::getObjectCount());
+
+        $id = $result->id;
+        $this->assertTrue($id > 1, 'id must be greater than 1');
+
+        $objectDirect = AbstractObject::getById($id);
+        $creationDate = $objectDirect->getCreationDate();
+
+        $this->assertTrue($creationDate >= $time, 'wrong creation date');
+
+        // as the object key is unique there must be exactly one object with that key
+        $list = $this->restClient->getObjectList('{ "o_key" : "' . $unsavedObject->getKey() . '"}');
+        $this->assertEquals(1, count($list));
+
+        $fetchedObject = $this->restClient->getObjectById($id);
+        $this->assertInstanceOf(GroupInterface::class, $fetchedObject);
+    }
+}
