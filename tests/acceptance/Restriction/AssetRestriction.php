b3f8868d41d01d20541eb0b9f6d1e7eee6e06036
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-06-30 11:06:59 +0200

    prepare members 2.5 sealing

---
+++ tests/acceptance/Restriction/AssetRestriction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DachcomBundle\Test\acceptance\ExtJs;
+
+use DachcomBundle\Test\AcceptanceTester;
+
+class AssetRestriction
+{
+
+    public function testSingleRestrictedAssetDownload(AcceptanceTester $I)
+    {
+    }
+
+    public function testMultipleRestrictedAssetDownload(AcceptanceTester $I)
+    {
+    }
+
+    public function testSingleRestrictedAssetDownloadWithoutAuthorization(AcceptanceTester $I)
+    {
+    }
+
+    public function testMultipleRestrictedAssetDownloadWithoutAuthorization(AcceptanceTester $I)
+    {
+    }
+}
