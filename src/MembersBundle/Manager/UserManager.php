d9c0f60b0fcb40f13bc9b3ac3ea02775d5f94a7c
dasraab <bs.privat@gmail.com>
2020-04-02 08:59:48 +0200

    only set Initial Groups if present (#135)
    Co-authored-by: bsc <bs@bitbird.de>

--- src/MembersBundle/Manager/UserManager.php
+++ src/MembersBundle/Manager/UserManager.php
@@ -262,7 +262,9 @@ class UserManager implements UserManagerInterface
             }
         }
 
-        $user->setGroups($userGroups);
+        if (count($userGroups)) {
+            $user->setGroups($userGroups);
+        }
 
         return $user;
     }
